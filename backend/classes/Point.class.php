f10ac60a3a1d7f156093ff7c3aedbc31ea3a4418
zagart <zagart.dev@mail.ru>
2016-09-28 12:44:20 +0300

    Back-end: частично реализован класс Point.

--- backend/classes/Point.class.php
+++ backend/classes/Point.class.php
@@ -1,1 +1,23 @@
 <?php
+
+class Point {
+	private $lat = 0; //id
+	private $lng = 0; //c
+	
+	public function __construct(float $lat, float $lng) {
+		$this -> lat = $lat;
+		$this -> lng = $lng;
+	}
+	
+	public function getLat() : float {
+		return $this -> lat;
+	}
+	
+	public function getLng() : float {
+		return $this -> lng;
+	}
+	
+	public function __destruct() {
+		//act on destruct
+	}
+}
