2fa5e224c74be0f2f02f57907d2b334c5acaea5a
zagart <zagart.dev@mail.ru>
2016-09-28 16:52:07 +0300

    Back-end: index.php теперь выводит эхом одно направление.

--- backend/index.php
+++ backend/index.php
@@ -2,6 +2,9 @@
 	require_once "lib".DIRECTORY_SEPARATOR."data.php";
 	define('DEFAULT_LAT_ERR', 0.00003);
 	define('DEFAULT_LNG_ERR', 0.00004);
+
+$directions;
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,9 +19,9 @@
 			printData(filterData(
 				$data,
 				(int) $routeType,
-				$data[2] -> t,
+				$data[0] -> t,
 				$data[4] -> t,
-				$data[2],
+				$data[0],
 				$data[4],
 				array ("lat" => DEFAULT_LAT_ERR, "lng" => DEFAULT_LNG_ERR)
 				)
@@ -34,14 +37,26 @@
 			echo "</h3><hr/>";
 		?>
 -->
+<!-- id - идентификатор навигатора
+n - номер маршрута
+с - тип транспорта (троллейбус 1 автобус 0 маршрутка 4)
+t - временная отметка положения транспорта Unix
+lat, lag - координаты
+a - азимут (угол)
+s - скорость транспорта -->
 		<?php
-			if ($_SERVER["REQUEST_METHOD"] == "POST") {
-				echo "<h1>REQUESTED POST METHOD.</h1>";
-				echo "<pre>";
-				print_r($_REQUEST);
-				echo "</pre>";
-			}
-			
+		$data = loadAllData("xxx.json");
+		$filteredData = filterData(
+				$data,
+				(int) $routeType,
+				$data[2] -> t,
+				$data[4] -> t,
+				$data[2],
+				$data[4],
+				array ("lat" => DEFAULT_LAT_ERR, "lng" => DEFAULT_LNG_ERR)
+				);
+		$directions = json_encode($filteredData);
+		echo $directions;
 		?>
 </body>
 </html>
