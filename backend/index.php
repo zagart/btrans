fbc95400fc6f7355e93b37fab51d8515108a4d42
zagart <zagart.dev@mail.ru>
2016-09-28 16:59:59 +0300

    Back-end: тестовый вывод.

--- backend/index.php
+++ backend/index.php
@@ -10,6 +10,28 @@
 	</head>
 	<body>
 		<h1>Data parser index.html</h1>
+		<?php 
+			$data = loadAllData("xxx.json");
+			printData(filterData(
+				$data,
+				(int) $routeType,
+				$data[0] -> t,
+				$data[4] -> t,
+				$data[0],
+				$data[4],
+				array ("lat" => DEFAULT_LAT_ERR, "lng" => DEFAULT_LNG_ERR)
+				)
+			);
+			$pointA = $data[0];
+			$pointA -> lat = 53.694051;
+			$pointA -> lng = 23.825847;
+			$pointB = $data[1];
+			$pointB -> lat = 53.693727;
+			$pointB -> lng = 23.827242;
+			echo "<h3>Test distance(~100m): ";
+			print(calculateDistance($pointA, $pointB));
+			echo "</h3><hr/>";
+		?>
 		<!-- id - идентификатор навигатора
 		n - номер маршрута
 		с - тип транспорта (троллейбус 1 автобус 0 маршрутка 4)
