8321e26b8e3969aa1c6c3a868fc26ef08987d227
vlad2503 <vlad2503@github.com>
2016-10-10 01:33:17 +0300

    Front-end: Получение данных напримере gpsID

--- frontend/index.php
+++ frontend/index.php
@@ -17,19 +17,27 @@
 			$.ajax({
 				type: 'POST',
 				url: "http://btrans/backend/index.php",
-				dataType: "html",
+				
 				data: $(this).serialize(),
-				success: function(html) {
-					$("#msg").html(html);
-					
+				success: function() {
+					$.get("http://btrans/backend/data.json", success, "json");
+				},
+				error: function() {
+					alert("Ошибка");
 				}
 			});
+			function success(data) {
+				var posts = [];
+				$.each(data,function(key, val){
+					posts.push('<p>'+val.gpsId+'</p>');
+				});
+				$('#msg').html(posts.join(''));
+			}
 			
-		});	
-		/*var date = document.getElementById("minTime").getAttribute("value");
+		/*var date = document.getElementById("Radius").getAttribute("value");
 		alert(date);*/
+		});
 	}
-	
 	</script>
 </head>
 <body>
@@ -47,14 +55,15 @@
 				<label>MaxTime</label><br>
 			</div>
 			<div class="inputForm">
-				<input class="ltta" type="text" name="latitudeA" /><br>
-				<input class="lgta" type="text" name="longitudeA" /><br>
-				<input class="lttb" type="text" name="latitudeB" /><br>
-				<input class="lgtb" type="text" name="longitudeB" /><br>
-				<input class="Radius" type="text" name="radius" /><br>
+				<input class="ltta" type="text" name="latitudeA" value="53.69798" /><br>
+				<input class="lgta" type="text" name="longitudeA" value="23.81959" /><br>
+				<input class="lttb" type="text" name="latitudeB" value="53.69264" /><br>
+				<input class="lgtb" type="text" name="longitudeB" value="23.8235" /><br>
+				<input class="Radius" type="text" name="radius" value="20" /><br>
+                
 				<input id="minTime" type="datetime-local" name="minTime"><br>
 				<input id="maxTime" type="datetime-local" name="maxTime"><br> 
-	<!--		<input class="quantity" type="text" name="quantity"><br>  -->
+				<input class="quantity" type="text" name="quantity"><br>  
 				
 			</div>
 			<div class="clear"></div>
@@ -62,6 +71,7 @@
 		
 	</fieldset>
 </form>
+	<canvas id="chart" width="600" height="400"></canvas>
 	<div id="msg"></div>
 </body>
 </html>
