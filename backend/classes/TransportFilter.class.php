68454f7f70c5e1ce6f705d95ac52429bce641d33
zagart <zagart.dev@mail.ru>
2016-10-10 23:25:10 +0300

    TransportFilter class released

--- backend/classes/TransportFilter.class.php
+++ backend/classes/TransportFilter.class.php
@@ -2,18 +2,46 @@
 
 class TransportFilter extends Filter {
 	
-	const BY_DELAY_TIME = 0;
+	const BY_TIMESTAMP = 0;
 	
-	public function applyFilters(array &$transport, array $constants) {
+	public function applyFilters(array &$transport, array $constants) : array {
 		foreach ($constants as $constant) {
-			if ($constant === BY_DELAY_TIME) {
-				filterByDelayTime($transport);
+			if ($constant === BY_TIMESTAMP) {
+				return $this -> filterByTimestamp($transport);
 			}
 		}
+		return array();
 	}
 	
-	private function filterByDelayTime(array &$transport) {
-		//do something
+	private function filterByTimestamp(array $transport) : array {
+		$multipliedTransportArray = array();
+		foreach ($transport as $singleTransport) {
+			$multipliedTransportArray[] = $this -> multiplyTransportByLocations($singleTransport);
+		}
+		$splittedMultipliedTransportArray = array();
+		foreach ($multipliedTransportArray as $multipliedTransport) {
+			foreach ($multipliedTransport as $transport) {
+				$splittedMultipliedTransportArray[] = $transport;
+			}
+		}
+		return $splittedMultipliedTransportArray;
+	}
+	
+	private function multiplyTransportByLocations(Transport $transport) : array {
+		$multipliedTransport = array();
+		$instance = array(
+			"id" => $transport -> getId(), 
+			"gpsId" => $transport -> getGpsNavigator() -> getId(), 
+			"route" => $transport -> getRoute(), 
+			"type" => $transport -> getType(),
+		);
+		$locations = $transport -> getGpsNavigator() -> getLocationsArchive();
+		foreach ($locations as $location) {
+			$newInstance = clone $instance;
+			$newInstance["location"] -> $location;
+			$multipliedTransport[] = $newInstance;
+		}
+		return $multipliedTransport;
 	}
 	
 }
