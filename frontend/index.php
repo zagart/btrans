052064eecc8b972512ab3709d71431ba70b14d72
vlad2503 <vlad2503@github.com>
2016-10-14 22:17:31 +0300

    Fornt-end: Added line chart

--- frontend/index.php
+++ frontend/index.php
@@ -6,18 +6,19 @@
 	<link rel="stylesheet" type="text/css" href="style/style.css" />
 	<title>SetJS</title>
 	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 	<script type="text/javascript">
 	
 		
+		var start_time = [];
+		var end_time = [];
 	
 	function getData() {
 		$('#myForm').submit(function(e){
 			e.preventDefault();
-			
 			$.ajax({
 				type: 'POST',
 				url: "http://btrans/backend/index.php",
-				
 				data: $(this).serialize(),
 				success: function() {
 					$.get("http://btrans/backend/data.json", success, "json");
@@ -26,17 +27,62 @@
 					alert("Ошибка");
 				}
 			});
-			function success(data) {
-				var posts = [];
-				$.each(data,function(key, val){
-					posts.push('<p>'+val.gpsId+'</p>');
-				});
-				$('#msg').html(posts.join(''));
-			}
-			
-		/*var date = document.getElementById("Radius").getAttribute("value");
-		alert(date);*/
 		});
+	}
+	function success(data) {
+		
+		$.each(data,function(key, val){
+			start_time.push(val.startLocation.timestamp * 1000 + 3 * 3600000);
+			end_time.push(val.endLocation.timestamp * 1000 + 3 * 3600000);
+			
+		});
+		google.charts.load('current', {packages: ['corechart', 'line']});
+		google.charts.setOnLoadCallback(drawBasic);
+		//$('#msg').html(end_time.join('<br>'));
+		//$('#msg').html(data);
+	}
+	function drawBasic() {
+	  var data = new google.visualization.DataTable();
+      data.addColumn('number', 'X');
+      data.addColumn('number', 'Минуты');
+	  
+	  for (var x = 0; x<start_time.length; x++) {
+		  for (var y = 0; y<end_time.length; y++) {
+			  start_time[x] = new Date(start_time[x] * 1000);
+			  end_time[y] = new Date(end_time[y] * 1000);
+			  data.addRows([
+				[start_time[x].getHours(), end_time[y].getHours()]
+			  ]);
+			  x++;
+		  }
+	  }
+      /*data.addRows([
+        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+        [66, 70], [67, 72], [68, 75], [69, 80]
+      ]);*/
+
+      var options = {
+        hAxis: {
+          title: 'Time'
+        },
+        vAxis: {
+          title: 'Popularity'
+        }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart'));
+
+      chart.draw(data, options);
 	}
 	</script>
 </head>
@@ -71,7 +117,6 @@
 		
 	</fieldset>
 </form>
-	<canvas id="chart" width="600" height="400"></canvas>
-	<div id="msg"></div>
+	<div id="chart"></div>
 </body>
 </html>
